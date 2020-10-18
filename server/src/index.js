bdfe57e0ad10b801b0592a7ed26c204e8216abb1
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-10-18 18:24:51 +0300

    feat(server): create simple Node:Express server

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 9000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`[app]: http://localhost:${port}`);
+});
