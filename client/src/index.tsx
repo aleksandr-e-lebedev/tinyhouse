41db3e2c57e5ec66dd23079fe897577560ff6bc1
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-11-21 18:53:43 +0300

    chore(client): create Apollo Client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import { Listings } from './sections';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: '/api',
+});
 
 const App = () => <Listings title="TinyHouse Listings" />;
 
 render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
