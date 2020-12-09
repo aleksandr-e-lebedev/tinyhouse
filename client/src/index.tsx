1b72f5bc052d3a3d722af63255bd8a028e68b65a
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-12-09 16:15:49 +0300

    feat(client): build UI for Login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { Layout } from 'antd';
 
-import { Home, Host, Listing, Listings, User, NotFound } from './sections';
+import {
+  Home,
+  Host,
+  Listing,
+  Listings,
+  Login,
+  User,
+  NotFound,
+} from './sections';
 
 import './styles/index.css';
 
@@ -14,26 +23,31 @@ const client = new ApolloClient({
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/host">
-        <Host />
-      </Route>
-      <Route exact path="/listing/:id">
-        <Listing />
-      </Route>
-      <Route exact path="/listings/:location?">
-        <Listings />
-      </Route>
-      <Route exact path="/user/:id">
-        <User />
-      </Route>
-      <Route>
-        <NotFound />
-      </Route>
-    </Switch>
+    <Layout className="app">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/host">
+          <Host />
+        </Route>
+        <Route exact path="/listing/:id">
+          <Listing />
+        </Route>
+        <Route exact path="/listings/:location?">
+          <Listings />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/user/:id">
+          <User />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 };
 
