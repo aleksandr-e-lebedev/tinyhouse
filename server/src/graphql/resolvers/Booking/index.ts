306135ba9e3987941c64ca402cf1f072dafe1525
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-12-24 19:35:31 +0300

    feat(server): handle querying listing data

--- server/src/graphql/resolvers/Booking/index.ts
+++ server/src/graphql/resolvers/Booking/index.ts
@@ -1,6 +1,6 @@
 import { IResolvers } from 'apollo-server-express';
 
-import { Database, Booking, Listing } from '../../../lib/types';
+import { Database, Booking, Listing, User } from '../../../lib/types';
 
 export const bookingResolvers: IResolvers = {
   Booking: {
@@ -23,6 +23,24 @@ export const bookingResolvers: IResolvers = {
       } catch (error) {
         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
         throw new Error(`Failed to query booking listing: ${error}`);
+      }
+    },
+    tenant: async (
+      booking: Booking,
+      _args: Record<string, unknown>,
+      { db }: { db: Database }
+    ): Promise<User> => {
+      try {
+        const tenant = await db.users.findOne({ _id: booking.tenant });
+
+        if (!tenant) {
+          throw new Error("Tenant can't be found");
+        }
+
+        return tenant;
+      } catch (error) {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        throw new Error(`Failed to query booking tenant: ${error}`);
       }
     },
   },
