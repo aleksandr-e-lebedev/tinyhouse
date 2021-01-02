9dd37266e6d3843337c9a024e4b1a77603ca4c8a
Aleksandr Lebedev <lebe.dev@outlook.com>
2021-01-02 18:16:25 +0300

    feat(client): handle searching for listings from App Header

--- client/src/sections/AppHeader/index.tsx
+++ client/src/sections/AppHeader/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Layout } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { Input, Layout } from 'antd';
 
 import { MenuItems } from './components';
 import { Viewer } from '../../lib/types';
+import { displayErrorMessage } from '../../lib/utils';
 
 import logo from './assets/tinyhouse-logo.png';
 
@@ -15,8 +16,39 @@ interface Props {
 }
 
 const { Header } = Layout;
+const { Search } = Input;
 
 export const AppHeader = ({ viewer, setViewer }: Props): JSX.Element => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const { pathname } = location;
+
+    if (!pathname.includes('/listings')) {
+      setSearch('');
+      return;
+    }
+
+    const pathnameSubstrings = pathname.split('/');
+
+    if (pathnameSubstrings.length === 3) {
+      setSearch(pathnameSubstrings[2]);
+    }
+  }, [location]);
+
+  const handleSearch = (value: string) => {
+    const trimmedValue = value.trim();
+
+    if (trimmedValue) {
+      history.push(`/listings/${trimmedValue}`);
+    } else {
+      void displayErrorMessage('Please enter a valid search!');
+    }
+  };
+
   return (
     <Header className="app-header">
       <div className="app-header__logo-search-section">
@@ -24,6 +56,15 @@ export const AppHeader = ({ viewer, setViewer }: Props): JSX.Element => {
           <Link to="/">
             <img src={logo} alt="App logo" />
           </Link>
+        </div>
+        <div className="app-header__search-input">
+          <Search
+            placeholder="Search 'San Fransisco'"
+            enterButton
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onSearch={handleSearch}
+          />
         </div>
       </div>
       <div className="app-header__menu-section">
