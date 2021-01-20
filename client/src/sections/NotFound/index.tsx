93ab29b970c692ebbb6414e5413387dfa891fb59
Aleksandr Lebedev <lebe.dev@outlook.com>
2021-01-20 23:24:06 +0300

    feat(client): build UI of Not Found page

--- client/src/sections/NotFound/index.tsx
+++ client/src/sections/NotFound/index.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Empty, Layout, Typography } from 'antd';
+
+import './styles/NotFound.css';
+
+const { Content } = Layout;
+const { Text } = Typography;
 
 export const NotFound = (): JSX.Element => {
-  return <h2>NotFound</h2>;
+  return (
+    <Content className="not-found">
+      <Empty
+        description={
+          <>
+            <Text className="not-found__description-title">
+              Uh oh! Something went wrong :(
+            </Text>
+            <Text className="not-found__description-subtitle">
+              The page you&apos;re looking for can&apos;t be found
+            </Text>
+          </>
+        }
+      />
+      <Link
+        to="/"
+        className="not-found__cta ant-btn ant-btn-primary ant-btn-lg"
+      >
+        Go to Home
+      </Link>
+    </Content>
+  );
 };
